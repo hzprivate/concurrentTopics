00c9fb47d1b9786b94240e0984ea8ab8289fb814
hzprivate <869932173@qq.com>
2020-08-10 01:13:34 +0800

    测试是否提交正常

--- SemaphoreDemo/src/CollectionDemo.java
+++ SemaphoreDemo/src/CollectionDemo.java
@@ -32,6 +32,6 @@ public class CollectionDemo {
             threadss[i].start();
             threadss[i].join();
         }
-        System.out.println("after remove "+list.size());
+        System.out.println("after  remove "+list.size());
     }
 }
